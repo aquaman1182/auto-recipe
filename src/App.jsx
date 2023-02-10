4d5744d750b4171661f931ed8dcec927bfc1a1b4
{ichigo0126} <{luckyshoma@gmail.com}>
2023-02-11 04:08:56 +0900

    tab-from

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { MyTab } from './components/Tab'
 
 function App() {
 
   return (
     <div className="App">
-    
+      <MyTab /> 
     </div>
   )
 }
---
+++ src/components/Tab.jsx
@@ -0,0 +1,201 @@
+import { Tab } from "@headlessui/react";
+import { useState } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const foodCategory = {
+    cereals: "米・雑穀・シリアル",
+    noodles: "麺類",
+    vegetables: "野菜",
+    marineProducts: "水産物・水産加工品",
+    meetProducts: "肉・肉加工品",
+    dairyProducts: "卵・チーズ・乳製品",
+    fruits: "果物"
+
+}
+
+export const MyTab = () => {
+  let [categories] = useState({
+    //米・雑穀・シリアル //
+    cereals: [
+      {
+        id: 1,
+        title: "Does drinking coffee make you smarter?",
+        date: "5h ago",
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: "2h ago",
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    //麺類//
+    noodles: [
+      {
+        id: 1,
+        title: "Is tech making coffee better or worse?",
+        date: "Jan 7",
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: "The most innovative things happening in coffee",
+        date: "Mar 19",
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+     //野菜//
+    vegetables: [
+      {
+        id: 1,
+        title: "Ask Me Anything: 10 answers to your questions about coffee",
+        date: "2d ago",
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: "4d ago",
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+    //　水産物・水産加工品　//
+    marineProducts: [
+        {
+          id: 1,
+          title: "Ask Me Anything: 10 answers to your questions about coffee",
+          date: "2d ago",
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: "4d ago",
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+      //肉・肉加工品
+      meetProducts: [
+        {
+          id: 1,
+          title: "Ask Me Anything: 10 answers to your questions about coffee",
+          date: "2d ago",
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: "4d ago",
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+    //　乳製品
+      dairyProducts: [
+        {
+          id: 1,
+          title: "Ask Me Anything: 10 answers to your questions about coffee",
+          date: "2d ago",
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: "4d ago",
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+    //　果物
+      fruits: [
+        {
+          id: 1,
+          title: "Ask Me Anything: 10 answers to your questions about coffee",
+          date: "2d ago",
+          commentCount: 9,
+          shareCount: 5,
+        },
+        {
+          id: 2,
+          title: "The worst advice we've ever heard about coffee",
+          date: "4d ago",
+          commentCount: 1,
+          shareCount: 2,
+        },
+      ],
+
+  });
+
+  return (
+    <div className="w-full px-2 py-16 sm:px-0">
+      <Tab.Group>
+        <Tab.List>
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames("tab tab-bordered", selected ? "tab-active" : "")
+              }
+            >
+              {foodCategory[category]}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                "rounded-xl bg-white p-3",
+                "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+              )}
+            >
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative rounded-md p-3 hover:bg-gray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.title}
+                    </h3>
+
+                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                      <li>{post.date}</li>
+                      <li>&middot;</li>
+                      <li>{post.commentCount} comments</li>
+                      <li>&middot;</li>
+                      <li>{post.shareCount} shares</li>
+                    </ul>
+
+                    <a
+                      href="#"
+                      className={classNames(
+                        "absolute inset-0 rounded-md",
+                        "ring-blue-400 focus:z-10 focus:outline-none focus:ring-2"
+                      )}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+};
