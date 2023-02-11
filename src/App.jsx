76ebb0dd8bf8cccebf7034d721bff2642aaa387c
aquaman1182 <hal.bandotakumi@gmail.com>
2023-02-11 18:14:16 +0900

    Recipe View

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import { MyTab } from "./components/Tab";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <MyTab />
-    </div>
+
+    </div >
   );
 }
 
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 
 export const Loading = () => {
+
+  
   return (
     <div className="text-center">
       <div role="status">
